9cb26e1b5187bf4a0f8b474c18f6e48f764c38ea
Jiri Lidinsky <jlidinsky@gmail.com>
2017-12-01 05:39:15 +0100

    Add an control application source file.

--- src/heat_components.py
+++ src/heat_components.py
@@ -1,17 +1,32 @@
+# -*- coding: utf-8 -*-
 
+class TQS3:
+    type = 'TQS3'
+    label = 'BT'
+    def __init__(self, spinel_address = 49):
+        pass
 
 class PT100:
     type = "PT100"
     short = "Thermistor"
     label = "BT"
+    def __init__(self, 
+        short_desc = None):
+
+        self.short_desc = short_desc
+        pass
+    
     
 class P5310:
     type = "P5310"
     short = "Temperature Current Converter"
+    # output range: 0-60 stC
 
 class Quido88:
     type = 'Quido 8/8'
     short = 'Binary IOs and temperature measurement'
+    def __init__(self, spinel_address = 49):
+        pass
     
 class AD4ETH:
     type = 'AD4ETH'
@@ -24,12 +39,12 @@ class DA2RS:
               a regulaci. Dva nezávislé analogové výstupy mohou být
               buď napěťové nebo proudové. Hodnoty pro analogové výstupy
               zasílá nadřazený systém přes rozhraní RS232 nebo RS485."""
-    gates = {
-        'out1' :
-        'out2' :
-        'comm' :
-        'pwrs' :
-    }
+#    gates = {
+#        'out1' :
+#        'out2' :
+#        'comm' :
+#        'pwrs' :
+#    }
     
 class GNOME:
     type = 'GNOME'
@@ -38,6 +53,7 @@ class GNOME:
 class SENSYCON:
     type = 'SENSYCON'
     short = 'PT100 to current converter'
+    # output range 0-100 dgC
     
 class Rele:
     type = 'Rele'
@@ -46,21 +62,27 @@ class Rele:
 components = {        
     
     # Čidlo pro měření teploty vody na výstupu z kotle
-    '=KK-BTH' : PT100(),
+    '=KK-BTH' : PT100(
+        short_desc = """Senzor teploty média na výstupu z krbových kamen."""
+    ),
     
     '-B3' : P5310(),
     
     '+204-BT' : TQS3(
         spinel_address = 12,
-        
-            
     ),
     
     '+0-BT' : TQS3(
         ),
         
-    '-B1' : Quido88(),
-    '-B2' : Quido88(),
+    '-B1' : Quido88(
+        spinel_address = 0x34 # 34h
+    ),
+
+    '-B2' : Quido88(
+        spinel_address = 0x35 # 35h
+    ),
+
     '-B3' : AD4ETH(),
     '-B4' : DA2RS(),
     '-B5' : GNOME(),
@@ -71,8 +93,15 @@ components = {
     '-K3' : Rele(),
     '-K4' : Rele(),
     '-K5' : Rele(),
+    '-K6' : Rele(),
+    '-K7' : Rele(),
+    '-K8' : Rele(),
+    '-K9' : Rele(),
+
 }        
 
 
 for c in components:
-    print('{} & {}\\'.format(c, components[c])
+    print('{} & {} & {}\\\\'.format(c, components[c].type, components[c].short_desc))
+
+
