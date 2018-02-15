9b09674aeade811cbb0f5eb5a1654ee9b15d3c45
Jiri Lidinsky <jlidinsky@gmail.com>
2018-02-15 21:16:27 +0100

    Work on schema class.

--- book/script/A1_sch.py
+++ book/script/A1_sch.py
@@ -1,39 +1,49 @@
-from Canvas import Square
+# -*- coding: utf-8 -*-
+
+from schema.canvas import Square
+from schema.canvas import Canvas
+from schema.vector import I
 
 class Scheme:
 
     def __init__(self, canvas):
         self.canvas = canvas
-        
+        self.t = I
+
+    def draw_rect(self, size):
+        Square(self.t.r_scale(size[0], size[1])).write(self.canvas)
+
 class ScaledScheme(Scheme):
 
     def __init__(self, canvas):
-    
+        pass
+
 
 class CabinetPosition:
-    
+
     def __init__(self, n_modules):
         self.n_modules = n_modules
-        self.content = List(n_modules)
-        
+        self.content = list(n_modules)
+
     def draw_face_view(self, scheme):
-        
+        pass
+
 
 class Cabinet:
 
     dimensions = (360.0, 540.0)
     n_positions = 3
-    n_modules = (16, 16, 16) 
-    
+    n_modules = (16, 16, 16)
+
     def __init__(self):
-        self.content = (CabinetPosition(self.n_modules[n]) for n in n_modules)
+        self.content = (CabinetPosition(self.n_modules[n]) for n in self.n_modules)
 
     def draw_face_view(self, scheme):
-        scheme.draw_rect(dimensions)
+        scheme.draw_rect(self.dimensions)
         scheme.pushmove(0, 120.0)
         for p in self.content:
             p.draw_face_view(scheme)
-            scheme.move(0,150.0)        
+            scheme.move(0,150.0)
 
 
 
@@ -42,13 +52,13 @@ class Quido88:
     dimensions = (137.4, 96.5, 20)
 
     def draw_face_view(self, scheme):
-        scheme.draw_rect(dimensions)
+        scheme.draw_rect(self.dimensions)
         scheme.draw_circle(3.2, (64.45, 40.0))
         scheme.draw_circle(3.2, (-64.45, 40.0))
         scheme.draw_circle(3.2, (64.45, -40.0))
         scheme.draw_circle(3.2, (-64.45, -40.0))
-            
-    
+
+
 
 
 # Vykres velikosti A3
@@ -60,7 +70,7 @@ RAW_PAPER_SIZE = (436.0, 313.0)
 
 
 draw_rect(OUTER_FRAME_SIZE) # draw outer frame
-draw_rect(GRID_FRAME_SIZE)  # 
+draw_rect(GRID_FRAME_SIZE)  #
 draw_rect(DRAWING_FRAME_SIZE)
 
 # draw center marks
@@ -74,17 +84,17 @@ draw_vline(0, -y, -size)
 
 # draw grid
 size = 0.5 * size
-dist = 0.5 * GRID_FRAME_SIZE[0] / 3   
+dist = 0.5 * GRID_FRAME_SIZE[0] / 3
 for i in range(2):
     draw_vline((i+1)*dist, y, size)
-    draw_vline(-dist*(i+1), y, size)                
+    draw_vline(-dist*(i+1), y, size)
     draw_vline((i+1)*dist, -y, -size)
-    draw_vline(-dist*(i+1), -y, -size)                
+    draw_vline(-dist*(i+1), -y, -size)
 
-dist = 0.5 * GRID_FRAME_SIZE[1] / 2   
+dist = 0.5 * GRID_FRAME_SIZE[1] / 2
 for i in range(1):
     draw_hline(x, (i+1)*dist, size)
-    draw_hline(x, -dist*(i+1), size)                
+    draw_hline(x, -dist*(i+1), size)
     draw_hline(-x, (i+1)*dist, -size)
     draw_hline(-x, -dist*(i+1), -size)
 
